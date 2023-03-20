6000f8af263c5543d52297d423919f670adb9b3b
devyanshugautam <devyanshu.20205065@mnnit.ac.in>
2023-03-20 17:14:17 +0530

    basic react app is updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Signup from "./screens/Signup";
+import { useState } from "react";
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+     <div>
+        <Routes>
+
+          <Route exact path="/" element={user && user._id ? (
+              <Home setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}/>
+          {/* <Route>{
+              user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} /> 
+            }</Route> */}
+          <Route exact path="/login" element={<Login setLoginUser={setLoginUser}/>}/>
+          <Route exact path="/creatuser" element={<Signup/>}/>
+          {/* <Route exact path="/">
+            {user && user._id ? (
+              <Home setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+       
+          <Route path="/creatuser" ><Signup/></Route>  */}
+        </Routes>
+        </div>
+    </Router>
+    
   );
 }
 
