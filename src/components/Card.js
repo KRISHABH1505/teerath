296f2e2500b26d57a0b245bf207ab36044a17085
devyanshugautam <devyanshu.20205065@mnnit.ac.in>
2023-03-27 11:02:58 +0530

    data stored in database is shown in each card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, {useEffect, useRef, useState}from "react";
+import { useDispatchCart,useCart } from "./ContextReducer";
+function Card(props) {
+let dispatch= useDispatchCart();
+  let data = useCart();
+  const priceRef = useRef();
+  let options = props.options;
+  let priceOptions = Object.keys(options);
+  const [qty, setQty] = useState(1);
+  const [size, setSize] = useState("");
+  const handleAddToCart =async ()=>{
+    let food = []
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
 
-function Card() {
+        break;
+      }
+    }
+    if(food!==[]){
+         if(food.size===size){
+          await dispatch({type:"UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty})
+          return
+         }
+         else if(food.size!==size){
+          await dispatch({type:"ADD", id:props.foodItem._id, name:props.foodItem.name, price: finalPrice, qty:qty, size:size})
+          return
+         }
+         return
+    }
+
+    await dispatch({type:"ADD", id:props.foodItem._id, name:props.foodItem.name, price: finalPrice, qty:qty, size:size})
+       
+       console.log(data);
+  }
+  let finalPrice = qty*parseInt(options[size]);
+  useEffect(()=>{
+    setSize(priceRef.current.value)
+  },[])
   return (
     <div>
-      <div>
-        <div>
           <div
             className="card mt-3"
             style={{ width: "18rem", maxHeight: "360px" }}
           >
             <img
-              src="https://www.istockphoto.com/photo/paneer-tikka-at-skewers-in-black-bowl-at-dark-slate-background-paneer-tikka-is-an-gm1186759790-334957865?phrase=paneer%20tikka"
+              // src={props.imgSrc}
+              // src={props.foodItem.img}
+              src='https://source.unsplash.com/random/900x700/?food'
               className="card-img-top"
               alt="..."
+              style={{height:"120px", objectFit:"fill"}}
             />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">this is specifications of food</p>
+              <h5 className="card-title">{props.foodItem.name}</h5>
+              {/* <p className="card-text">this is specifications of food</p> */}
               <div className="container w-100">
-                <select className="m-2 h-100 bg-success">
+                <select className="m-2 h-100 bg-success"  onChange={(e)=> setQty(e.target.value)}>
                   {Array.from(Array(6), (e, i) => {
                     return (
                       <option key={i + 1} value={i + 1}>
@@ -27,16 +64,17 @@ function Card() {
                     );
                   })}
                 </select>
-                <select className="m-2 h-100 bg-success">
-                  <option value="half">half</option>
-                  <option value="full">full</option>
+                <select className="m-2 h-100 bg-success rounded" ref={priceRef} onChange={(e)=> setSize(e.target.value)}>
+                  {priceOptions.map((data)=>{
+                    return <option key={data} value={data}>{data}</option>
+                  })}
                 </select>
-                <div className="d-inline h-100 fs-5">Total Price</div>
+                <div className="d-inline h-100 fs-5">₹{finalPrice}/-</div>
+                <hr></hr>
+                <button className={`btn btn-success justify-centre ms-2`} onClick={handleAddToCart}>Add to Cart</button>
               </div>
             </div>
           </div>
-        </div>
-      </div>
     </div>
   );
 }
