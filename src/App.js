59e3f92d3263544100fdb50be3e3953ce43dc078
devyanshugautam <devyanshu.20205065@mnnit.ac.in>
2023-03-27 11:01:17 +0530

    route for My orders is creted in App.js file of frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import MyOrder from "./screens/MyOrder";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
@@ -8,11 +9,12 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Signup from "./screens/Signup";
 import { useState } from "react";
+import { CartProvider } from "./components/ContextReducer";
 
 function App() {
   const [user, setLoginUser] = useState({});
   return (
-    
+    <CartProvider>
     <Router>
      <div>
         <Routes>
@@ -20,13 +22,14 @@ function App() {
           <Route exact path="/" element={user && user._id ? (
               <Home setLoginUser={setLoginUser} />
             ) : (
-              <Login setLoginUser={setLoginUser} />
+              <Home setLoginUser={setLoginUser} />
             )}/>
           {/* <Route>{
               user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} /> 
             }</Route> */}
           <Route exact path="/login" element={<Login setLoginUser={setLoginUser}/>}/>
           <Route exact path="/creatuser" element={<Signup/>}/>
+          <Route exact path="/myOrder" element={<MyOrder/>}/>
           {/* <Route exact path="/">
             {user && user._id ? (
               <Home setLoginUser={setLoginUser} />
@@ -42,7 +45,7 @@ function App() {
         </Routes>
         </div>
     </Router>
-    
+    </CartProvider>
   );
 }
 
